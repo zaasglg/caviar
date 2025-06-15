84bd297fc7b941ca27711fd3840432e457a44108
zaasglg <n4msin@mail.ru>
2025-06-15 19:56:37 +0500

    fix errror

--- app/Livewire/Sections/OfferSection.php
+++ app/Livewire/Sections/OfferSection.php
@@ -9,9 +9,14 @@ use Illuminate\Support\Facades\Cache;
 class OfferSection extends Component
 {
     public $promotions = [];
+    public $color = 'black';
+    public $single = false;
 
-    public function mount()
+    public function mount($color = 'black', $single = false)
     {
+        $this->color = $color;
+        $this->single = $single;
+        
         $this->promotions = Cache::remember('active_promotions', 3600, function () {
             return Promotion::where('is_active', true)->get();
         });
@@ -20,7 +25,9 @@ class OfferSection extends Component
     public function render()
     {
         return view('livewire.sections.offer-section', [
-            'promotions' => $this->promotions
+            'promotions' => $this->promotions,
+            'color' => $this->color,
+            'single' => $this->single
         ]);
     }
 }
